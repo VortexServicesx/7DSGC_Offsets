6d4dc89e01df50e45f58d58548ab5679da41e978
Avexy <jackosbornlot@gmail.com>
2026-04-23 19:39:25 -0400

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -2,7 +2,7 @@
 
 // Offsets will update every time the game updates. If the game updates, the offsets won’t be updated until I have time to update them on GoFile.
 
-// visit this GoFile for updated offsets, because the file is 36MB and GitHub only allows 25MB:
+// visit this GoFile for updated offsets, because the file is large and GitHub only allows 25MB:
 // https://gofile.io/d/SorYch
 
 // If the .hpp file is not opening, try opening it in an IDE to view it properly.
