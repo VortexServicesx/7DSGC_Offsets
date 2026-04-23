2314027d0aa736e7b8e2134ec02a316dc91f62b6
Avexy <jackosbornlot@gmail.com>
2026-04-23 19:46:51 -0400

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -3,9 +3,9 @@
 // UPDATES
 // Offsets will update every time the game updates. If the game updates, the offsets won’t be updated until I have time to update them on GoFile.
 
-// OFFSETS -- DONT READ
+// OFFSETS 
 // visit this GoFile for updated offsets, and i cant provide the offsets here unfortunately because the file is large and GitHub only allows 25MB:
-// OFFSETS HERE: https://gofile.io/d/SorYch
+// OFFSETS HERE: https://gofile.io/d/SorYch or https://raw.githubusercontent.com/VortexServicesx/7DSGC_Offsets/refs/heads/main/output.hpp
 
 // IMPORTANT
 // If the .hpp file is not opening, try opening it in an IDE to view it properly.
