dbfcc9bb014d1656889eac9052028317973fa9da
Avexy <jackosbornlot@gmail.com>
2026-04-24 14:53:33 -0400

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -5,12 +5,12 @@ Offsets will update every time the game updates. If the game updates, the offset
 
 // PC CLIENT OFFSETS 
 visit this GoFile for updated offsets, and i cant provide the offsets here unfortunately because the file is large and GitHub only allows 25MB:
-OFFSETS HERE: https://gofile.io/d/O0BONj
+OFFSETS HERE: https://gofile.io/d/RrQRMI
 
 
 // ANDROID OFFSETS
 visit this GoFile for updated offsets, and i cant provide the offsets here unfortunately because the file is large and GitHub only allows 25MB:
-OFFSETS HERE: https://gofile.io/d/8z7djd
+OFFSETS HERE: https://gofile.io/d/RrQRMI
 
 // IMPORTANT - READ
 If the .hpp file is not opening, try opening it in an IDE to view it properly.
